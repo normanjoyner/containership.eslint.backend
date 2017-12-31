4f499ab6abb35b7c94ade281c6bac847056e404f
Norman Joyner <norman.joyner@gmail.com>
2017-12-31 16:58:25 -0500

    add experimentalObjectRestSpread

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ module.exports = {
         "node": true
     },
     "parserOptions": {
-        "ecmaVersion": 2017
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
     },
     "extends": "eslint:recommended",
     "rules": {
